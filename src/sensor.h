3381d896e4bc89548056ff783b54f43a8474b824
Elia Carrara <johny.chanta@gmail.com>
2016-04-08 16:15:52 +0200

    started the long quest of documenting...

--- src/sensor.h
+++ src/sensor.h
@@ -1,23 +1,33 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+/// \file   sensor.h
+/// \author Elia Carrara
+/// \date   April, 2016
+/// \brief  Contains the definition of the sensor base class.
 
-/// This namespace contains all sensor related items.
+
+/// \brief This namespace contains all sensor related items.
 namespace Sensor{
 
-///
-/// Base class for all sensors. This class represents the base class for all sensors.
-///
+/// \brief This class represents the base class for sensors.
+/// \details SensorBase represents the base of all sensor classes.
+/// This class uses two template types (S,T). S should be used for
+/// a type defined in si_types.h, T should be used with a raw data type
+/// whiche are defined in classes inheriting from SensorBase.
 template <class S, class T>
 class SensorBase {
     public:
-        ///
-        /// \brief ReadSensorDataOnce
-        /// \param OutData
-        /// \return
-        ///
+        /// \brief Reads sensor data.
+        /// \param ReadSensorDataOnce should be implemented to read
+        /// raw data from a sensor. The argument is passed by reference. This
+        /// means it will store the gatherd data after it's successful completion
+        /// \return Returns true when it succeeded.
         virtual bool ReadSensorDataOnce(T & OutData)=0;
-
+        /// \brief Converts raw sensor data to a SI unit.
+        /// \param ConvertToSIUnit should be implemented to convert raw data(T)
+        /// provided by a sensor to an SI unit(S).
+        /// \return Returns an SI unit.
         virtual S ConvertToSIUnit(T Data)=0;
 };
 
--- src/si_types.h
+++ src/si_types.h
@@ -1,9 +1,21 @@
 #ifndef SI_TYPES_H
 #define SI_TYPES_H
 
+/// \file   si_types.h
+/// \author Elia Carrara
+/// \date   April, 2016
+/// \brief  Contains typedefs with names for SI units.
+/// \details These representations shoud make it easier to work with.
+/// Rather than getting a "float" from a function, it is way
+/// more simpler to know what for a si unit you are dealing with.
+
+/// \brief Represensation for a Celcius unit.
 typedef float Celcius;
+/// \brief Represensation for a Gauss unit.
 typedef float Gauss;
+/// \brief Represensation for a degree per second unit.
 typedef float DPS;
+/// \brief Represensation for a g (force) unit.
 typedef float GForge;
 
 #endif
